d25470ffb843c3b917fe6e1554a36011c6b6674c
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2022-11-30 02:44:12 +0600

    Add files via upload

---
+++ Quiz_App/QuizApp/Controller/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by Md Murad Hossain on 22/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var playButton: UIButton! {
+        didSet {
+            playButton.layer.cornerRadius = playButton.frame.height/2
+        }
+    }
+    @IBOutlet weak var topicButton: UIButton! {
+        didSet {
+            topicButton.layer.cornerRadius = topicButton.frame.height/2
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onClickPlay(_ sender: Any) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "QuizViewController") as? QuizViewController else {return}
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func onClickTopic(_ sender: Any) {
+    }
+}
+
